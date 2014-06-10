52adee283a022a0c871731da0160a13ae3357f8d
Matthew Stringer <matt@proyecto-lacumbre.com>
2014-06-10 15:21:27 -0500

    file submitted

--- code/data_prep/add_features.py
+++ code/data_prep/add_features.py
@@ -1,38 +1,66 @@
 import json
 
-features = json.load(open('C:\Users\Prateek Raj\Desktop\houston_analysis\houston_short.json', 'r'))
-
-res='C:/Users/Prateek Raj/Desktop/houston_analysis/data/building_res.txt'
-fres=open(res,'r')
+import argparse
+import textwrap
 
 
-other='C:/Users/Prateek Raj/Desktop/houston_analysis/data/building_other.txt'
-
-fother=open(other,'r')
 
 
-def match_property(prop_id):
-	p = None
-	print 'called'
-	fres.seek(0)
-
-	for prop in fres:
-		p = prop.split('\t')
-		# print prop_id, p[0].replace(' ','')
-		if p[0].replace(' ','') == prop_id:
-
-			print p[0], prop_id
-			break
-
-	
-			
-	return p
 
 
-for num in range(len(features)):
-	prop_id = features[num]['properties']["HCAD_NUM"]
-
-	p = match_property(prop_id)
 
 
-fres.close()
+class checkFile:
+
+	def __init__(self,res,other, features):
+		self.res = self.open_file(res)
+		self.other = self.open_file(other)
+		self.features = self.open_json(features)
+
+
+	def open_file(self, file_name):
+		f = open(file_name,'r')
+		return f
+
+	def open_json(self, file_name):
+		features = json.load(open(file_name, 'r'))
+		return features
+
+
+	def run_features(self):
+		print "running"
+		for num in range(len(self.features)):
+
+			prop_id = self.features[num]['properties']["HCAD_NUM"]
+
+			p = self.match_property(prop_id)
+
+	def match_property(self, prop_id):
+		p = None
+		print 'called'
+		self.res.seek(0)
+		for prop in self.res:
+			p = prop.split('\t')
+
+			print prop_id, p[0].replace(' ','')
+			if p[0].replace(' ','') == prop_id:
+				print p[0], prop_id
+				break
+		return p
+
+
+
+
+def main():
+	other = '/Users/mattstringer/research/Houston_analysis/data/text_folder/building_other.txt'
+	res = '/Users/mattstringer/research/Houston_analysis/data/text_folder/building_res.txt'
+	features = '/Users/mattstringer/research/Houston_analysis/houston_short.json'
+
+	c = checkFile(res, other, features)
+	c.run_features()
+
+
+
+
+if __name__ == '__main__':
+	main()
